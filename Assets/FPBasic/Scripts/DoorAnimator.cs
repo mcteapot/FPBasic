1ab373a2c78c1cedcd926d9a43b7f16326004807
Arjun Prakash <archieoi@gmail.com>
2016-10-24 11:28:30 -0700

    door open added

---
+++ Assets/FPBasic/Scripts/DoorAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimator : MonoBehaviour {
+
+	public Animator doorAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		Debug.Log("TRIGGER ENTERED");
+		doorAnimator.SetTrigger("open");
+
+	}
+}
---
+++ Assets/FPBasic/Scripts/DoorOpener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour {
+
+	public Transform door;
+
+	private float startPos;
+	public float endPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = door.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		Debug.Log("TRIGGER ENTERED");
+		door.position = new Vector3(door.position.x, door.position.y, endPos);
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log("TRIGGER EXIT");
+
+		door.position = new Vector3(door.position.x, door.position.y, startPos);
+	}
+}
